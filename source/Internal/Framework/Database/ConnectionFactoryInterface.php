342c962899e2f0a5ab2986e5cbdbf5b979f904e8
Vasyl Liulka <vasyl.liulka@oxid-esales.com>
2024-04-11 13:59:20 +0200

    OXDEV-7248 Switch DB configs [WIP]

--- source/Internal/Framework/Database/ConnectionFactoryInterface.php
+++ source/Internal/Framework/Database/ConnectionFactoryInterface.php
@@ -9,7 +9,14 @@ namespace OxidEsales\EshopCommunity\Internal\Framework\Database;
 
 use Doctrine\DBAL\Driver\Connection;
 
+<<<<<<<< HEAD:source/Internal/Framework/Database/ConnectionFactoryInterface.php
 interface ConnectionFactoryInterface
+========
+/**
+ * @deprecated will be removed in next major
+ */
+interface ConnectionProviderInterface
+>>>>>>>> 88915c445 (OXDEV-7248 Switch DB configs [WIP]):source/Internal/Framework/Database/ConnectionProviderInterface.php
 {
     public function create(): Connection;
 }
